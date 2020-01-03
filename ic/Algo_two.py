e0f22ba2a74904c2824bf509267844dfa1c425ae
Myriam19 <meriamzek@hotmail.com>
2020-01-03 21:09:53 +0100

    Version fonctionnelle v1
    algo fonctionnel mais non optimisé

--- ic/Algo_two.py
+++ ic/Algo_two.py
@@ -4,14 +4,24 @@
 # V1.0 2020/01/01 - Python 3.7
 
 from ic.Vertex import Vertex
+from ic.Edge import Edge
 from ic.Graph import Graph
 from collections import Counter
 from operator import itemgetter
 
+import time
+
 def compute(k: int, delta : int, sg: list) -> list:
     init(k, delta, sg)
-    tree1 = get_tree(sub_graphs[3])
-    print(tree1)
+    tree1 = get_tree(sub_graphs[0])
+    tree2 = get_tree(sub_graphs[1])
+    tree3 = get_tree(sub_graphs[2])
+    tree4 = get_tree(sub_graphs[3])
+    result._add_sub_graph_(tree1)
+    result._add_sub_graph_(tree2)
+    result._add_sub_graph_(tree3)
+    result._add_sub_graph_(tree4)
+    print(result)
 
 def init(k: int, delta : int, sg: list) :
     global sub_graphs
@@ -20,37 +30,83 @@ def init(k: int, delta : int, sg: list) :
     sub_graphs = sg
     big_graph = get_all_sub_graphs_in_one()
     result = Graph("Result")
+    result._reset_()
     print(str(big_graph))
 
 def get_all_sub_graphs_in_one() -> Graph:
-    for g in sub_graphs :
-        print(g)
     return Graph("Global", sub_graphs=sub_graphs)
 
 def get_tree(graph : Graph) -> Graph :
     tree = Graph("Tree")
+    tree._reset_()
+    edge = get_edge_with_max_weight(graph._edges, tree)
+    tree._add_edge_(e=edge)
+    #print("first edge : {}".format(edge))
 
-    weight = big_graph._get_edge_(graph._edges[0]._v1, graph._edges[0]._v2)._weight
-    edge = graph._edges[0]
-    for i in range (1, len(graph._edges)):
-        if big_graph._get_edge_(graph._edges[i]._v1, graph._edges[i]._v2)._weight > weight :
-            weight = big_graph._get_edge_(graph._edges[i]._v1, graph._edges[i]._v2)._weight
-            edge = graph._edges[i]
-    
-    tree._add_edge_(edge._v1, edge._v2)
+    v1 = edge._v1
+    v2 = edge._v2
 
-    start = edge._v2
-    change = True
+    v1_edges = graph._get_edges_from_vertex(v1)
+    v2_edges = graph._get_edges_from_vertex(v2)
+
+    best_edge_v1 = get_edge_with_max_weight(v1_edges, tree)
+    best_edge_v2 = get_edge_with_max_weight(v2_edges, tree)
+    #print("best v1 edge : {}".format(best_edge_v1))
+    #print("best v2 edge : {}".format(best_edge_v2))
+    #time.sleep(5)
+
+    start = None
+    best_edge = None
+    if best_edge_v1 :
+        start = v1
+        best_edge = best_edge_v1
+    if (not start) or (best_edge_v2 and best_edge_v2._weight > best_edge_v1._weight): 
+        start = v2
+        best_edge = best_edge_v2
+
+    change = False
+    #print("best choosen edge : {}".format(best_edge))
+    if start and best_edge :
+        tree._add_edge_(e=best_edge)
+        start = best_edge._get_other_vertex_(start)
+        change = True
 
     while change :
         change = False
         edges_start = graph._get_edges_from_vertex(start)
-        for e in edges_start :
-            end = e._get_other_vertex_(start)
-            if not tree._has_vertex_(end) :
-                tree._add_edge_(e._v1, e._v2)
-                start = end
-                change = True
-                break
+        best_edge = get_edge_with_max_weight(edges_start, tree)
+        #print("best edge : {}".format(best_edge))
+        if best_edge :
+            tree._add_edge_(e=best_edge)
+            start = best_edge._get_other_vertex_(start)
+            change = True
 
     return tree
+
+def get_edge_with_max_weight(edges : list, tree : Graph) -> Edge :
+    #print("tree : {}".format(tree))
+    edge = None
+    if edges :
+        i=0
+        while not edge and i < len(edges):
+            #"print("choosing first one")
+            #print("v1 {} : {}".format(edges[i]._v1, tree._has_vertex_(edges[i]._v1)))
+            #print("v2 {} : {}".format(edges[i]._v2, tree._has_vertex_(edges[i]._v2)))
+            #print("*****")
+            #time.sleep(1)
+            if not tree :
+                edge = edges[i]
+            elif not tree._has_vertex_(edges[i]._v1) or not tree._has_vertex_(edges[i]._v2) :
+                edge = edges[i]
+            i += 1
+
+        while i < len(edges) :
+            #print("compare v1 {} : {}".format(edges[i]._v1, tree._has_vertex_(edges[i]._v1)))
+            #print("compare v2 {} : {}".format(edges[i]._v2, tree._has_vertex_(edges[i]._v2)))
+            #print("*****")
+            if not tree or (not tree._has_vertex_(edges[i]._v1) or not tree._has_vertex_(edges[i]._v2)) :
+                if edges[i]._weight > edge._weight :
+                    edge = edges[i]
+                    #print("choose !")
+            i += 1
+    return edge
--- ic/Graph.py
+++ ic/Graph.py
@@ -34,6 +34,7 @@ class Graph(object):
         if sub_graphs :
             for sg in sub_graphs :
                 self._add_sub_graph_(sg)
+            self._update_sub_graphs_weights_(sub_graphs)
         else :
             self._is_complet = is_complet
 
@@ -43,12 +44,16 @@ class Graph(object):
             if is_complet :
                 for i in range (0,len(self._vertices)-1) :
                     for j in range (i+1, len(self._vertices)):
-                        self._add_edge_(self._vertices[i], self._vertices[j])
+                        self._add_edge_(v1=self._vertices[i], v2=self._vertices[j])
             else :
                 self._edges = edges
         
 
-    def _add_edge_(self, v1 : Vertex, v2 : Vertex, compute_weight : bool = False):
+    def _add_edge_(self, v1 : Vertex = None, v2 : Vertex = None, e : Edge = None, compute_weight : bool = False):
+        if e :
+            v1 = e._v1
+            v2 = e._v2
+
         if not self._has_edge_(v1, v2) :
             self._add_vertex_(v1)
             self._add_vertex_(v2)
@@ -63,6 +68,8 @@ class Graph(object):
 
 
     def _add_vertex_(self, v : Vertex) :
+        if not v :
+            return
         if not self._has_vertex_(v) :
             self._vertices.append(Vertex(v._tag))
 
@@ -74,9 +81,16 @@ class Graph(object):
         for v in graph._vertices :
             self._add_vertex_(v)
         for e in graph._edges :
-            self._add_edge_(e._v1, e._v2, True)
+            self._add_edge_(e=e, compute_weight=True)
+
+    def _update_sub_graphs_weights_(self, sub_graphs : list) :
+        for sg in sub_graphs :
+            for edge in sg._edges :
+                edge._weight = self._get_edge_(edge._v1, edge._v2)._weight
 
     def _get_vertex_(self, v:Vertex) -> Vertex:
+        if not v :
+            return None
         for vertex in self._vertices :
             if(v._tag == vertex._tag) :
                 return vertex
@@ -96,6 +110,10 @@ class Graph(object):
         return edges
 
     def _has_edge_(self, v1 : Vertex, v2 : Vertex) -> bool :
+        if not v1 : 
+            return False
+        if not v2 :
+            return False
         for edge in self._edges :
             if edge._is_edge_of_(v1, v2) :
                 return True
@@ -109,6 +127,10 @@ class Graph(object):
                 if vertex._tag == v._tag :
                     return True
         return False
+
+    def _reset_(self) :
+        self._vertices = []
+        self._edges = []
 
     def __hash__(self):
         return hash(id(self))
